9790831d45d89b02c78e90bca4e23292c6a02b14
jeffrey.zksun@gmail.com <jeffrey.zksun@gmail.com@ef2b5007-3930-7ef5-57b6-99d0e7df5f01>
2011-06-05 05:55:56 +0000

    Add XmlNodeWrapper

--- trunk/Brain/Main/XmlIOStream.cpp
+++ trunk/Brain/Main/XmlIOStream.cpp
@@ -1,7 +1,7 @@
 #include "StdAfx.h"
 #include "XmlIOStream.h"
 
-XmlIOStream::XmlIOStream(bool bIsRead) : mbIsRead(bIsRead)
+XmlIOStream::XmlIOStream(bool bIsRead) : mbIsRead(bIsRead), mpCurrentNode(NULL), mDocumentWrapper()
 {
 }
 
@@ -12,13 +12,36 @@ void XmlIOStream::Clear()
 {
 
 }
+
+bool XmlIOStream::BeginNode(const CString& nodeName)
+{
+	if(!mbIsRead)
+	{
+		//CXmlNodeWrapper nodeWrapper(nodeName);
+	}
+
+	return false;
+}
+bool XmlIOStream::CloseNode()
+{
+	// Back to parent
+	return false;
+}
+
+bool XmlIOStream::SetNodeValue(const CString& nodeName)
+{
+	return false;
+}
+
 bool XmlIOStream::Load(const CString& docName)
 {
+	//return mDocumentWrapper.Load(docName);
 	return false;
 }
 
 bool XmlIOStream::Save(const CString& docName)
 {
+	//return mDocumentWrapper.Save(docName);
 	return false;
 }
 
