edc98628f45cb79d06ea4deb5a04396ab719a6a6
Jeffrey.zksun <jeffrey.zksun@gmail.com>
2011-06-06 07:52:14 +0000

    Add AnalyzeInputOptions. Print help.

--- PC/PC.cpp
+++ PC/PC.cpp
@@ -3,20 +3,65 @@
 
 #include "stdafx.h"
 #include <list>
+#include <iostream>
 
 #include "DocumentManager.h"
 #include "TaskManager.h"
 
+void AnalyzeInputOptions(int argc, _TCHAR* argv[], ParameterObject& options);
+void PrintHelp();
+
 // Define the supported parameter names.
-#define XMLFILE _T("File")
+#define OPT_FILE _T("file") // Read FILE as the task schema.
+#define OPT_HELP _T("help")
 
 // The command line likes PC.exe File="C:\_DemoPcUsage.xml"
 int _tmain(int argc, _TCHAR* argv[])
 {
-	ParameterObject parameters;
+	ParameterObject options;
+
+	AnalyzeInputOptions(argc, argv, options);
+
+	bool bShowHelp = false;
+	if(options.GetParameterLength() == 0)
+	{
+		bShowHelp = true;
+
+	}
+	else if (options.GetParameterLength() == 1)
+	{
+		Parameter para;
+		bool bHasItem = options.GetParameter(OPT_HELP, para);
+		if(bHasItem)
+			bShowHelp = true;
+	}
+
+	if(bShowHelp)
+	{
+		PrintHelp();
+		return 0;
+	}
+
+	Parameter fileName;
+	bool bRet = options.GetParameter(OPT_FILE, fileName);
+	if(!bRet)
+	{
+		PrintHelp();
+		return 1;
+	}
+
+	DocumentManager* pDoc = new DocumentManager();
+	pDoc->SetDocumentName(fileName.GetEvaluatedValue());
+	pDoc->XmlIn();
+	TaskManager::Get()->RunTasks();
+	delete pDoc;
+
+	return 0;
+}
+
+void AnalyzeInputOptions(int argc, _TCHAR* argv[], ParameterObject& options)
+{
 	// The first parameter is the command name itself.
-	bool hasLocalISFolder = false;
-	bool hasISVersion = false;
 	CString str;
 	for(int i = 1; i < argc; i++)
 	{
@@ -24,7 +69,14 @@ int _tmain(int argc, _TCHAR* argv[])
 		str = argv[i];
 		int pos = str.Find(_T('='));
 		if (-1 == pos) // not find
+		{
+			// Check if it's help;
+			if(str.CompareNoCase(_T("\\?")) == 0 || str.CompareNoCase(_T("-?")) == 0)
+			{
+				options.AddParameter(Parameter(OPT_HELP, _T("")));
+			}
 			continue;
+		}
 
 		// Verify name is not empty
 		CString varName = str.Left(pos);
@@ -38,23 +90,18 @@ int _tmain(int argc, _TCHAR* argv[])
 
 		// Add the parameter
 		Parameter varPara(varName, varValue);
-		parameters.AddParameter(varPara);
+		options.AddParameter(varPara);
 	}
+}
 
-	Parameter xmlFileName;
-	bool bRet = parameters.GetParameter(XMLFILE, xmlFileName);
-	ASSERT(bRet);
-	if(!bRet)
-	{
-		return 1;
-	}
-
-	DocumentManager* pDoc = new DocumentManager();
-	pDoc->SetDocumentName(xmlFileName.GetEvaluatedValue());
-	pDoc->XmlIn();
-	TaskManager::Get()->RunTasks();
-	delete pDoc;
-
-	return 0;
+void PrintHelp()
+{
+	std::cout << "Usage: PC [options...]\n";
+	std::cout << "Options:\n";
+	std::cout << "	-?, /?			Show help.\n";
+	std::cout << "	file=FILE		Read FILE as the task schema.\n";
+	std::cout << "\n";
+	std::cout << "This program built for i386-pc\n";
+	std::cout << "Report bugs to http://code.google.com/p/pcassist/issues/list\n";
 }
 
