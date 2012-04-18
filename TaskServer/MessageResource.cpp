28762d131c50bc8fb93fbbf6f857771d603a5bd4
Jeffrey.zksun <jeffrey.zksun@gmail.com>
2012-04-18 09:41:20 +0000

    update

--- TaskServer/MessageResource.cpp
+++ TaskServer/MessageResource.cpp
@@ -6,18 +6,8 @@ const WString& MessageResource::GetWelcomeMesage()
 	static WString msg =	
 		_T("**********************************************\r\n")
 		_T("*                                            *\r\n")
-		_T("*     Weclome to Task Automation Server      *\r\n")
+		_T("*     Weclome to Task Automation System      *\r\n")
 		_T("*                                            *\r\n")
 		_T("**********************************************\r\n");
-	return msg;
-}
-
-const WString& MessageResource::GetTaskPromptMesage()
-{
-	static WString msg =	
-		_T("\r\n")
-		_T("\r\n")
-		_T("Allowed taks:\r\n")
-		_T("\t-T dwg -V G037 -N objectdbx_g053.100_2_3_2012\r\n");
 	return msg;
 }
